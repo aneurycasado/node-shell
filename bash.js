6c95be4dd3d574c7ebf1e19ede1e820473269471
Kathy Lu <kaffy.lu@gmail.com>
2015-08-04 15:00:39 -0400

    started piping (unfinished)

--- bash.js
+++ bash.js
@@ -9,53 +9,64 @@ var done = function(output)
   process.stdout.write('\nprompt > ');
 }
 
+var 
+
 process.stdin.on('data', function(data)
 {
-  var inputArray = data.toString().trim().split(" ");
-  var cmd = inputArray[0]; // remove the newline
-  var args = inputArray.slice(1);
-  if(cmd === "pwd")
+  var inputArray = data.toString().trim().split("|");
+  var inputBetweenPipes = inputArray.map(function(element)
   {
-     commands.pwd("", done);
-  }
-  else if(cmd === "date")
-  {
-    commands.date("", done);
-  }
-  else if(cmd === "ls")
-  {
-     commands.ls("", done);
-  }
-  else if(cmd === "echo")
-  {
-    commands.echo(args, done);
-  }
-  else if(cmd === "cat")
-  {
-    commands.cat(args[0], done);
-  }
-  else if(cmd === "head")
-  {
-    commands.head(args[0], done);
-  }
-  else if(cmd === "tail")
-  {
-    commands.tail(args[0], done);
-  }
-  else if(cmd === "sort")
-  {
-    commands.sortFile(args[0], done);
-  }
-  else if(cmd === "wc")
-  {
-    commands.wc(args[0], done);
-  }
-  else if(cmd === "uniq")
-  {
-    commands.uniq(args[0], done);
-  }
-  else if(cmd === "curl")
-  {
-    commands.curl(args[0], done);
-  }
+    return element.trim();
+  });
+
+  var firstThing = inputBetweenPipes[0].split(" ");
+  var cmd = firstThing[0];
+  var args = firstThing.slice(1);
+
+  console.log(inputArray);
+
+  // if(cmd === "pwd")
+  // {
+  //    commands.pwd("", done);
+  // }
+  // else if(cmd === "date")
+  // {
+  //   commands.date("", done);
+  // }
+  // else if(cmd === "ls")
+  // {
+  //    commands.ls("", done);
+  // }
+  // else if(cmd === "echo")
+  // {
+  //   commands.echo(args, done);
+  // }
+  // else if(cmd === "cat")
+  // {
+  //   commands.cat(args[0], done);
+  // }
+  // else if(cmd === "head")
+  // {
+  //   commands.head(args[0], done);
+  // }
+  // else if(cmd === "tail")
+  // {
+  //   commands.tail(args[0], done);
+  // }
+  // else if(cmd === "sort")
+  // {
+  //   commands.sortFile(args[0], done);
+  // }
+  // else if(cmd === "wc")
+  // {
+  //   commands.wc(args[0], done);
+  // }
+  // else if(cmd === "uniq")
+  // {
+  //   commands.uniq(args[0], done);
+  // }
+  // else if(cmd === "curl")
+  // {
+  //   commands.curl(args[0], done);
+  // }
 });
